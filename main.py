522b8997eef1c0c348b40af7d74fb82870a1d729
JHauser48 <jacobhauser48@gmail.com>
2019-02-21 13:30:58 -0500

    static folder and moved CSS

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# maps URLs
-int x = 9
+from flask import Flask
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return redirect("/static/index.html", code=302)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
