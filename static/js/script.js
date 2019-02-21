2c2de5ec786a78fedf3d6b76950e1e3c6902d3d6
jmeaster30 <jmeasterday@comcast.net>
2019-02-21 17:48:45 -0500

    Started button functionality. Moved js to static folder so it'll actually work.

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+	$('#joindm').click(function(){
+		console.log("That doesn't work goofball.");
+	});
+	
+	$('#joinp').click(function(){
+		console.log("Joining " + $('#roomname').val() + "...");
+		//do joining room stuff
+	});
+});
