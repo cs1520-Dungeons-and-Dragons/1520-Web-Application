2a3bde4ebe8411a41f2dc68f92e8f0f7e3ac6f9e
paranoidandroid81 <mpk44@pitt.edu>
2019-03-09 16:15:49 -0500

    Created test branch. Modified for WebSockets. apparently working?

--- main.py
+++ main.py
@@ -1,32 +1,91 @@
 from flask import Flask, url_for, redirect, render_template, request
 from flask import session, jsonify
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_sockets import Sockets
 import random
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'memeslol'
 
-socketio = SocketIO(app)             # create socket listener
+sockets = Sockets(app)             # create socket listener
+#u_to_r = {}                  # map users to rooms
+r_to_client = {}                # map room to list of Clients connected`(uses Object from gevent API)
+last_client = []            # use to store previous clients list, compare to track clients
+
+# helper to roll dice, takes dice type and adv/disadv attributes
+def roll_dice(size, adv, dis, uname):
+  r1 = random.randint(1, size)
+  if (adv != dis):
+    # if distinct values, means rolled 2 dice
+    r2 = random.randint(1, size)
+    msg = ('(d' + str(size) + '): ' + uname + ' rolled ' + str(r1) + ' and ' + str(r2) + 
+    ' with ' + ('advantage' if adv else 'disadvantage') + ': use roll ' 
+    + (str(max(r1, r2)) if adv else str(min(r1, r2))))
+  else:
+    # just 1 roll
+    msg = '(d' + str(size) + '): ' + uname + ' rolled a ' + str(r1)
+  return msg
+
+# helper for when new client enters room, track
+def add_client(clients, room):
+  # take set difference of new list of clients and old
+  # difference should be one new client added
+  global last_client 
+  global r_to_client
+  new_client = list(set(clients) - set(last_client))
+  if room not in r_to_client.keys():
+    r_to_client[room] = []  # if empty, create new list
+  r_to_client[room].append(new_client[0]) # append first element in collection, new client
+  last_client = clients # save new client list
+
+# helper to determine what type of request based on header, form response
+def decide_request(req, uname, clients, room):
+  resp = ""
+  req_type = req['type']
+  if req_type == 'enter':
+    # person has joined room, must take difference of new clients list and old
+    # use to track person in room
+    add_client(clients, room)
+    resp = {'msg': uname + ' has entered the battle!', 'color': 'red', 'type': 'status'}
+  elif req_type == 'text':
+    # someone is sending a message
+    resp = {'msg': uname + ': ' + req['msg'], 'color': 'blue', 'type': 'chat'}
+  elif req_type == 'dice_roll':
+    # someone is asking for dice rolls
+    msg = roll_dice(int(req['dice_type']), req['adv'], req['disadv'], uname)
+    resp = {'msg': msg, 'color':'green', 'weight':'bold', 'type': 'roll'}
+  return json.dumps(resp) # convert JSON to string
+
 
 
 # begin listening for different socket events
 
-# on client joining room
-@socketio.on('joined', namespace='/play')
-def handle_join(data):
-    room = session.get('room')
+# on client joining chat room, process request and decide how to form response
+@sockets.route('/play')
+def chat_socket(ws):
+  # while socket is open, process messages
+  while not ws.closed:
+    message = ws.receive()
+    if message is None:  # message is "None" if the client has closed.
+      continue
+    # store name of sender
     uname = session.get('name')
-    #print (room)            # DEBUG
-    #print (uname)            # DEBUG
-    join_room(room)
-    emit('status', {'msg': uname + ' has entered the battle!', 'color': 'red'}, room=room)
+    global r_to_client
+    msg = json.loads(message) # convert to dict
+    # now process message dependent on type + room, clients
+    clients = list(ws.handler.server.clients.values())
+    room = session.get('room')
+    resp = decide_request(msg, uname, clients, room)
+    # send response to every one in sender's room
+    # filter out clients that aren't in room
+    #in_room = list(filter(lambda x: x.address in r_to_ip[room], clients))
+    for client in r_to_client[room]:
+      print("sending")
+      print(resp)
+      client.ws.send(resp)
+    
 
-# process new chat message from client
-@socketio.on('text', namespace='/play')
-def handle_text(message):
-    room = session.get('room')
-    uname = session.get('name')
-    emit('message', {'msg': uname + ': ' + message['msg'], 'color': 'blue'}, room=room)
+'''
 
 # on client leaving room
 @socketio.on('left', namespace='/play')
@@ -36,6 +95,7 @@ def handle_leave(message):
     leave_room(room)
     emit('status', {'msg': uname + ' has left the battle!', 'color': 'red'}, room=room)
 
+'''
 @app.route('/')
 def root():
     return redirect("/static/index.html", code=302)
@@ -51,32 +111,38 @@ def play():
 # redirect them to play url
 @app.route('/joinRoom', methods=['POST'])
 def join_post():
-    session['name'] = request.form['uname']
-    session['room'] = request.form['rname']
-    #msg = session.get('name') + ' joined room: ' + session.get('room')
-    #print(msg)
-    return redirect(url_for('.play'), code=302)
+  # store session info for use
+  session['name'] = request.form['uname']
+  session['room'] = request.form['rname']
+  #rname = request.form['rname']
+  '''
+  # store IP, port tuple of client connected to room
+  client_tup = (request.remote_addr, int(request.environ.get('REMOTE_PORT')))
+  print(client_tup) # DEBUG
+  if rname in r_to_ip.keys():
+    r_to_ip[rname].append(client_tup)
+  else:
+    r_to_ip[rname] = []
+    r_to_ip[rname].append(client_tup)
+  #msg = session.get('name') + ' joined room: ' + session.get('room')
+  #print(msg)
+  '''
+  return redirect(url_for('.play'), code=302)
 
 # disabling caching by modifying headers of each response
 @app.after_request
 def add_header(resp):
-    resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    resp.headers['Pragma'] = 'no-cache'
-    resp.headers['Expires'] = '0'
-    return resp
+  resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  resp.headers['Pragma'] = 'no-cache'
+  resp.headers['Expires'] = '0'
+  return resp
 
-#dice roll function for d4,6,8,10,12,20
-@socketio.on('dice_roll', namespace='/play')
-def dice_roll(data):
-    roll = random.randint(1, int(data['dice_type']))
-    roll2 = -1
-    msg = '(d' + str(data['dice_type']) + '): ' + session.get('name') + ' rolled a ' + str(roll)
-    if(data['adv'] != data['disadv']):
-        roll2 = random.randint(1, int(data['dice_type']))
-        msg = ('(d' + str(data['dice_type']) + '): ' + session.get('name') + ' rolled ' + str(roll) + ' and ' + str(roll2) + ' with ' + ('advantage' if data['adv'] else 'disadvantage') + ': use roll ' 
-        + (str(max(roll, roll2)) if data['adv'] else str(min(roll, roll2))))
 
-    emit('roll', {'msg': msg, 'color':'green', 'weight':'bold'}, room=session.get('room'))
-   
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+  print("""
+  This can not be run directly because the Flask development server does not
+  support web sockets. Instead, use gunicorn:
+
+  gunicorn -b 127.0.0.1:8080 -k flask_sockets.worker main:app
+
+  """)
--- static/js/play.js
+++ static/js/play.js
@@ -1,57 +1,82 @@
 var socket;
+var uname;
 console.log("before doc");
 $(document).ready(function(){
     console.log("doc loaded");
-	socket = io.connect('https://' + document.domain + ':' + location.port + '/play');
-	socket.on('connect', function(){
-		console.log("socket connected");
-		socket.emit('joined', {});
-	});
-	
-	socket.on('status', function(data){
-		//console.log(data);
-		$('#chatlog').append('<p style=\'color:' + data.color + '\'>&lt;' + data.msg + '&gt;</p>');
-		$('#chatlog').scrollTop($('#chatlog')[0].scrollHeight);
-	});
-	
-	socket.on('message', function(data){
-		//console.log(data);
-		$('#chatlog').append('<p style=\'color:' + data.color + '\'>' + data.msg + '</p>');
-		$('#chatlog').scrollTop($('#chatlog')[0].scrollHeight);
-	});
+    //store uname for this client
+    uname = $('#curr_user').text().split(', ')[1];
+    // if https, must be wss, otherwise ws
+    var scheme = window.location.protocol == "https:" ? 'wss://' : 'ws://';
+    var socket_uri = scheme + window.location.hostname + ':' + location.port + '/play';
+    socket = new WebSocket(socket_uri);         //create socket for URI
 
-    socket.on('roll', function(data){
-        $('#chatlog').append('<p style=\'color:' + data.color + ';' + 'font-weight:' + data.weight +'\'>' + data.msg + '</p>');
-		$('#chatlog').scrollTop($('#chatlog')[0].scrollHeight);
-    });
-	
-	$('#text').keypress(function(e){
-		var code = e.keyCode || e.which;
-		if(code == 13)
-		{
-			var t = $('#text').val();
-			$('#text').val('');
-			//erase all script tags
-			var SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
-			while (SCRIPT_REGEX.test(t)) {
-				t = t.replace(SCRIPT_REGEX, "");
-			}
-			if(t !== "") socket.emit('text', {msg: t});
-		}
-    });
-	
-	$('#leave').click(function(){
-		socket.emit('left', {}, function(){
-			socket.disconnect();
-			window.location.href = "/static/index.html";
-		});
-	});
+    // begin event handlers for socket
+    //when new connection opened, should send type: enter
+    socket.onopen = function(){
+	    console.log("opened socket");
+      // store in JSON for easy Python parsing
+      let msg = JSON.stringify({type: 'enter'});
+      socket.send(msg);
+    };
+    
+    //handle receipt of messages, behavior changes based on type
+    socket.onmessage = function(msg){
+      data = JSON.parse(msg.data); //convert to JS object
+      switch (data.type) {
+        case 'status':
+          $('#chatlog').append('<p style=\'color:' + data.color + '\'>&lt;' + data.msg + '&gt;</p>');
+          $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight);
+          break;
+        case 'chat':
+          $('#chatlog').append('<p style=\'color:' + data.color + '\'>' + data.msg + '</p>');
+          $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight);
+          break;
+        case 'roll':
+          $('#chatlog').append('<p style=\'color:' + data.color + ';' + 'font-weight:' + data.weight +'\'>' + data.msg + '</p>');
+          $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight);
+          break;
+      }
+    }
 
+    //handle if user sends message
+    $('#text').keypress(function(e){
+		  var code = e.keyCode || e.which;
+		  if(code == 13)
+		  {
+			  var t = $('#text').val();
+			  $('#text').val('');
+			  //erase all script tags
+			  var SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+			  while (SCRIPT_REGEX.test(t)) {
+				  t = t.replace(SCRIPT_REGEX, "");
+        }
+        // create string from chat text appended with type
+        let msg = JSON.stringify({type: 'text', msg: t});
+			  if(t !== "") socket.send(msg);
+		  }
+    });
+
+    //handle if user asks for dice roll
     $('#dice_roll').click(function(){
-        var adv, disadv;
-        $('#adv:checked').val() == "on" ? adv = true: adv = false;
-        $('#disadv:checked').val() == "on" ? disadv = true: disadv = false;
-        socket.emit('dice_roll', {dice_type: $('#dice_list').val(), adv: adv, disadv: disadv});
+      var adv, disadv;
+      // 1, 0 used to represent true, false respectively for advantage and disadvantage
+      adv = $('#adv:checked').val() == "on" ? 1 : 0;
+      disadv = $('#disadv:checked').val() == "on" ? 1 : 0;
+      // create string from type appended with dice info
+      let msg = JSON.stringify({type: 'dice_roll', dice_type: $('#dice_list').val(), adv: adv, disadv: disadv});
+      socket.send(msg);
     });
+    /*
+
+	
+	
+	  $('#leave').click(function(){
+		  socket.emit('left', {}, function(){
+			  socket.disconnect();
+			  window.location.href = "/static/index.html";
+		  });
+	  });
+
+    */
 
 });
